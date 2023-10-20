9d34f831f3b5dfd726f9b842edf01e04231e1f95
Kai Lam <kailamnyc@gmail.com>
2023-10-20 00:10:01 -0400

    updating config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
+if (
+	process.env.LD_LIBRARY_PATH == null ||
+	!process.env.LD_LIBRARY_PATH.includes(
+		`${process.env.PWD}/node_modules/canvas/build/Release:`
+	)
+) {
+	process.env.LD_LIBRARY_PATH = `${
+		process.env.PWD
+	}/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
+}
 
-const nextConfig = {
-	// reactStrictMode: false,
-	webpack: (config) => {
-		config.externals.push({
-			"utf-8-validate": "commonjs utf-8-validate",
-			bufferutil: "commonjs bufferutil",
-			canvas: "commonjs canvas",
-		});
-		// config.infrastructureLogging = { debug: /PackFileCache/ };
-		return config;
-	},
+module.exports = {
+	reactStrictMode: true,
 };
-
-module.exports = nextConfig;
