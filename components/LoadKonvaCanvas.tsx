785c99a2354422eb94273140e629686c07f28f9a
Kai Lam <kailamnyc@gmail.com>
2023-10-19 20:06:45 -0400

    finished view ui

--- components/LoadKonvaCanvas.tsx
+++ components/LoadKonvaCanvas.tsx
@@ -4,6 +4,23 @@ import axios from "axios";
 import Konva from "konva";
 import Image from "next/image";
 import React, { useEffect, useRef, useState } from "react";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "./ui/card";
+import {
+	Table,
+	TableBody,
+	TableCaption,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "./ui/table";
 
 type Props = {
 	reportId: string;
@@ -17,6 +34,10 @@ function LoadKonvaCanvas({ reportId }: Props) {
 			return data.data.payload;
 		},
 	});
+	const formatDate = (isoString) => {
+		const date = new Date(isoString);
+		return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+	};
 
 	useEffect(() => {
 		if (data?.stageState && containerRef.current) {
@@ -42,11 +63,53 @@ function LoadKonvaCanvas({ reportId }: Props) {
 					height: "612px", // Adjust based on your image's dimensions
 				}}
 			></div>
-			<h1>Author: {data.author.name}</h1>
-			<h1>Time of Report: {data.createdAt}</h1>
-			<h1>Patient Name: {data.patientName}</h1>
-			<h1>Patient Age: {data.patientAge}</h1>
-			<h1>Time of Injury: {data.timeOfInjury}</h1>
+			{/* <div className='flex justify-center items-center'> */}
+			<Card className='flex flex-col justify-center items-center'>
+				<CardHeader className='flex flex-col items-center'>
+					<CardTitle>{data.patientName}</CardTitle>
+					<CardDescription> {data.patientAge} years old</CardDescription>
+				</CardHeader>
+
+				<CardContent>
+					<p className='mb-2'>
+						<span className='font-semibold text-primary'>Author:</span>{" "}
+						{data.author.name}
+					</p>
+					<p className='mb-2'>
+						<span className='font-semibold text-primary'>Time of Report:</span>{" "}
+						{formatDate(data.createdAt)}
+					</p>
+
+					<p className='mb-2'>
+						<span className='font-semibold text-primary'>Time of Injury:</span>{" "}
+						{formatDate(data.timeOfInjury)}
+					</p>
+				</CardContent>
+			</Card>
+			{/* </div> */}
+
+			{/* <h1>Author: {data.author.name}</h1> */}
+
+			<Table className='gap-1'>
+				<TableCaption>List of {data.patientName} injuries</TableCaption>
+				<TableHeader>
+					<TableRow>
+						<TableHead className=''>Body Part</TableHead>
+						<TableHead>Description</TableHead>
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					{data.injuries.map((injury, index) => (
+						<TableRow key={index}>
+							{/* <TableCell className='font-bold'>{index + 1}</TableCell> */}
+							<TableCell className='font-semibold'>
+								{injury.locationOfInjury}
+							</TableCell>
+							<TableCell className=''>{injury.description}</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
 		</div>
 	);
 }
